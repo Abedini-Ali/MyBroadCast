2c59f7ac5935fa76c2935593da74240647fb6833
Ali <ali.abedini36@gmail.com>
2023-06-22 11:32:00 +0330

    1

--- app/src/main/java/com/ali/mybroadcast/MainActivity.kt
+++ app/src/main/java/com/ali/mybroadcast/MainActivity.kt
@@ -4,6 +4,7 @@ import android.content.Intent
 import android.content.IntentFilter
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
@@ -13,22 +14,29 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val intFilter = IntentFilter()
-        intFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-        this.registerReceiver(br, intFilter)
+//        val intFilter = IntentFilter()
+//        intFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+//        this.registerReceiver(br, intFilter)
 
     }
 
     override fun onStart() {
         super.onStart()
-//        val intFilter = IntentFilter()
-//        intFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
-//        this.registerReceiver(br, intFilter)
+        val intFilter = IntentFilter()
+        intFilter.addAction(Intent.ACTION_AIRPLANE_MODE_CHANGED)
+        this.registerReceiver(br, intFilter)
     }
 
     override fun onStop() {
         super.onStop()
-//        this.unregisterReceiver(br)
+        this.unregisterReceiver(br)
+    }
+
+    override fun onResume() {
+        super.onResume()
+//        Toast.makeText(this, "asdasdasdasd", Toast.LENGTH_SHORT).show()
+        onStop()
+        onStart()
     }
 
 }
